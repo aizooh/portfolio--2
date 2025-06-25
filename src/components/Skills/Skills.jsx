9be596533b6d28f67a942544ae35ea7195af3f4d
isaack <isaackaris.52@gmail.com>
2025-06-25 22:52:04 +0300

    skills added timestamp:47:44

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Skills.css';
 import {SKILLS} from "../../utilis/data";
 import SkillsCard from "./SkillCard/SkillCard";
-
+import  SkillsInfoCard from './SkillsInfoCard/SkillsInfoCard'
 
 const Skills = () => {
+    const [selectedSkill, SetSelectedSkill]= useState(SKILLS[0]);
+    const handleSelectedSkill= (data)=>{
+        SetSelectedSkill(data);
+    };
   return (
     <section className="skills-container">
         <h5> Technical Proficiency</h5>
@@ -15,10 +19,19 @@ const Skills = () => {
                     key={item.title}
                     iconUrl={item.icon}
                     title={item.title}
+                    isActive={selectedSkill.title === item.title}
+                    onClick={()=>{
+                        handleSelectedSkill(item);
+                    }}
                     />
                 ))}
             </div>
-            <div className="skills-info"></div>
+            <div className="skills-info">
+
+                <SkillsInfoCard 
+                heading={selectedSkill.title}
+                skills={selectedSkill.skills}  />
+            </div>
 
         </div>
     </section>
