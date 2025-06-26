cb1972409cf5a12fe186b7ea9813c2999cc32fa9
isaack <isaackaris.52@gmail.com>
2025-06-26 13:16:17 +0300

    contact section added next footer

---
+++ src/components/ContactMe/ContactMe.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './ContactMe.css'
+import ContactForm from './ContactForm/ContactForm'
+import ContactInfoCard from "./ContactInfoCard/ContactInfoCard"
+const ContactMe = () => {
+  return (
+    <section className="contact-container">
+
+        <h5>Contact Me</h5>
+        <div className="contact-content">
+            <div style={{ flex=1 }}>
+                <ContactInfoCard
+                iconUrl="./assets/images/01.png"
+                text="isaack@gmail.com" />
+                <ContactInfoCard
+                iconUrl="./assets/images/02.png"
+                text="https://github.com/aizooh" />
+            </div>
+            <div style={{ flex=1 }}>
+                <ContactForm/>
+            </div>
+        </div > 
+    </section>
+  )
+}
+
+export default ContactMe
