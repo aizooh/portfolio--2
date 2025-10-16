27e609959ada1853d0838804143923523510c027
isaack karanjah <isaackaris.52@gmail.com>
2025-10-16 10:15:25 +0300

    page sorted

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css';
-import AnalyticsTracker from 'src/AnalyticsTracker';
+// import AnalyticsTracker from './AnalyticsTracker';
 import App from './App.jsx';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -9,6 +9,6 @@ import "slick-carousel/slick/slick-theme.css";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <AnalyticsTracker />
+   
   </StrictMode>,
 )
